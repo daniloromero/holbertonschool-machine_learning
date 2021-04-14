2863d3c400fc16d0e1b4a36a51221cf72f3e4b06
danilormero <1601@holbertonschool.com>
2021-04-14 16:19:24 -0500

    Recursive unction to get shape of matrix

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
-import numpy as np
+"""calculates the shape of a matrix"""
+
+
+def len_matrix(matrix, shape):
+    """recursion to get matrix lenght"""
+    if isinstance(matrix, list):
+        shape.append(len(matrix))
+        if (len(matrix) > 0)
+            return len_matrix(matrix[0], shape)
+    else:
+        return shape
 
 def matrix_shape(matrix):
      """shape of a matrix"""
-     shape = np.shape(matrix)
-     return list(shape)
+     shape = []
+     if isinstance(matrix, list):
+        return len_matrix(matrix, shape)
+     # return shape
