b6c032cb54a94562d1335388a3f6e48216cde89e
danilormero <1601@holbertonschool.com>
2021-04-14 18:54:43 -0500

    Transpose a matrix

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""returns the transpose of a 2D matrix"""
+""" Returns the transpose of a 2D matrix """
 
 
 def matrix_transpose(matrix):
-    """ returns the transpose of a 2D matrix"""
+    """ Returns the transpose of a 2D matrix """
     transpose =[]
     if len(matrix) > 0:
         for i in range(len(matrix[0])):
