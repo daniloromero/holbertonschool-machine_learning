80c58536d3c504a17641bed354c288ca2a6f2cf4
danilormero <1601@holbertonschool.com>
2021-04-14 19:03:15 -0500

    Adds 2 arrays of the same shape

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
-import numpy as np
+""" Adds two arrays element-wise """
+
 
 def add_arrays(arr1, arr2):
+    """ Adds two arrays element-wise """
+    array_sum =[]
     if len(arr1) == len(arr2):
-        return np.add(arr1, arr2)
+        for i in range(len(arr1)):
+            array_sum.append(arr1[i] + arr2[i])
+        return array_sum
     else:
         return None
