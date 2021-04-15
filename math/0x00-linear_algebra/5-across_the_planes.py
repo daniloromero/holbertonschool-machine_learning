c0ca6fad562d6dee3ca6334fd06d5ad6eb630f57
danilormero <1601@holbertonschool.com>
2021-04-14 19:24:01 -0500

    Adds 2D matrices of the same shape

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
-import numpy as np
+""" Adds two matrices element-wise """
+
 
 def add_matrices2D(mat1, mat2):
-    if np.shape(mat1) == np.shape(mat2):
-        return np.add(mat1, mat2).tolist()
+    """ Adds two matrices element-wise """
+    mat_sum =[]
+    if len(mat1) == len(mat2) and len(mat1[0]) == len(mat2[0]):
+        for i in range(len(mat1)):
+            for j in range(len(mat1[0])):
+                mat_sum.append(mat1[i][j] + mat2[i][j])
+        return mat_sum
     else:
         return None
