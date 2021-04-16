ac71089936fa5b1e075735cd9608772050a5a80f
danilormero <1601@holbertonschool.com>
2021-04-16 00:49:55 -0500

    Add each dimension of matrix

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -7,8 +7,10 @@ def add_matrices2D(mat1, mat2):
     mat_sum = []
     if len(mat1) == len(mat2) and len(mat1[0]) == len(mat2[0]):
         for i in range(len(mat1)):
+            new = []
             for j in range(len(mat1[0])):
-                mat_sum.append(mat1[i][j] + mat2[i][j])
+                new.append(mat1[i][j] + mat2[i][j])
+            mat_sum.append(new)
         return mat_sum
     else:
         return None
