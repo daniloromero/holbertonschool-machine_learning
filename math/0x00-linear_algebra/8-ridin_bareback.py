eb57c7b2221307d280cf28abee54715e6a0d6afd
danilormero <1601@holbertonschool.com>
2021-04-18 20:28:05 -0500

    2D Matrix multiplication

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -4,13 +4,15 @@
 
 def mat_mul(mat1, mat2):
     """ function that performs matrix multiplication """
+    if len(mat1[0]) != len(mat2):
+        return None
     mul_mat = []
-    for i in range(len(mat1)):
+    for i, row in enumerate(mat1):
         new = []
         for j in range(len(mat2[0])):
             total = 0
             for k in range(len(mat1[0])):
-                total += mat1[i][k] * mat2[k][j]
+                total += row[k] * mat2[k][j]
             new.append(total)
         mul_mat.append(new)
     return (mul_mat)
