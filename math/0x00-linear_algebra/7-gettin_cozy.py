df4f13fc7f7e5cb93eb67fc6d23afb4158b1f059
danilormero <1601@holbertonschool.com>
2021-04-18 20:27:34 -0500

    Concatenation 2D matrices

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
-import numpy as np
-
+""" concatenates two matrices along a specific axis """
 
 def cat_matrices2D(mat1, mat2, axis=0):
-    return np.concatenate((mat1, mat2), axis).tolist()
+    """ concatenates two matrices along a specific axis """
+    if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
+        return mat1 + mat2
+    if axis == 1:
+        if len(mat1) != len(mat2):
+            return None
+        cat_mat = []
+        for i in range(len(mat1)):
+            m = mat1[i] + mat2[i]
+            cat_mat.append(m)
+    return cat_mat 
