8dfec9bd670d56b18cc8c7e48cdcf58a63a4b381
daniloromero <1601@holbertonschool.com>
2021-04-19 03:02:05 +0000

    Polinomial derivative as a list

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" calculates the derivative of a polynomial """
+
+
+def poly_derivative(poly):
+    """ calculates the derivative of a polynomial """
+    if isinstance(poly, list):
+        derivative = []
+        for i in range(len(poly)):
+            derivative.append(poly[i] * i)
+        if len(derivative) > 1:
+            return derivative[1:]
+        else:
+            return derivative
+    else:
+        return None
