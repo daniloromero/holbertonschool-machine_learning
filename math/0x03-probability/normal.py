fe43f5489e45806cf056b3d876f776db03a46203
daniloromero <1601@holbertonschool.com>
2021-04-21 02:11:25 +0000

    Added methodCalculates the x-value of a given z-score

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -31,3 +31,7 @@ class Normal:
     def z_score(self, x):
         """Calculates the z-score of a given x-value"""
         return (x - self.mean)/self.stddev
+
+    def x_value(self, z):
+        """Calculates the x-value of a given z-score """
+        return (z * self.stddev) + self.mean
