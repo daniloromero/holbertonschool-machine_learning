b5b99d01fa0a2bda7f99348837740f77d3d7eec8
daniloromero <1601@holbertonschool.com>
2021-05-07 23:21:29 +0000

    Adde property decorator for rivate attributes

---
+++ supervised_learning/0x01-classification/17-deep_neural_network.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+""" Module defines a deep neural network performing binary classification"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """class for deep neural network performing binary classification """
+
+    @staticmethod
+    def Weights_init(nx, layers):
+        """ Het-at-al Initialization of Weigths"""
+        weights_I = {}
+        for l in range(len(layers)):
+            if type(layers[l]) is not int or layers[l] < 1:
+                raise TypeError('layers must be a list of positive integers')
+            prev_layer = layers[l - 1] if l > 0 else nx
+            weights_I.update({
+                'W' + str(l): np.random.randn(
+                    layers[l], prev_layer) * np.sqrt(2/prev_layer),
+                'b' + str(l): np.zeros((layers[l], 1))})
+        return weights_I
+
+    def __init__(self, nx, layers):
+        """ Class constructor"""
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if type(layers) is not list or len(layers) == 0:
+            raise TypeError('layers must be a list of positive integers')
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = self.Weights_init(nx, layers)
+
+    @property
+    def L(self):
+        """ Layers getter """
+        return self.__L
+
+    @property
+    def cache(self):
+        """ Cache getter """
+        return self.__cache
+
+    @property
+    def weights(self):
+        """ weights getter """
+        return self.__weights
