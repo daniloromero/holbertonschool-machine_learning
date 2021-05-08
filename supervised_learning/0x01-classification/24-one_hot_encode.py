367ef4e23c090e4ba3122e75ca6004eac0754f95
daniloromero <1601@holbertonschool.com>
2021-05-08 10:41:14 +0000

    Added edgecase for type of classes

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -5,6 +5,8 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """ converts a numeric label vector into a one-hot matrix"""
+    if type(classes) is not int or classes <= 0:
+        return None
     n = max(Y)
     Z = np.zeros((classes, (n + 1)))
     for i in range(len(Y)):
