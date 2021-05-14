d3958f4976a7ad5b60fe0c2d78012cd57b2b366b
daniloromero <1601@holbertonschool.com>
2021-05-14 13:22:11 +0000

    FIX correction n variable for len(Y)

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -4,10 +4,16 @@ import numpy as np
 
 
 def one_hot_encode(Y, classes):
-    """ converts a numeric label vector into a one-hot matrix"""
+    """ converts a numeric label vector into a one-hot matrix
+    Args:
+        Y: is a numpy.ndarray with shape (m,) containing numeric class labels
+        m: is the number of examples
+        classes: is the maximum number of classes found in Y
+    Returns: a one-hot encoding of Y with shape (classes, m), or None on failure
+    """
     if type(classes) is not int or classes <= 0:
         return None
-    n = max(Y)
+    n = len(Y)
     try:
         Z = np.zeros((classes, (n + 1)))
         for i in range(len(Y)):
