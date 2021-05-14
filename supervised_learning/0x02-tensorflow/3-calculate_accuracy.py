fddf8a0feb29e11275449ca86da6e17c8987e4f6
daniloromero <1601@holbertonschool.com>
2021-05-14 12:39:47 +0000

    Added tf.equal on y and y_pred

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -10,6 +10,6 @@ def calculate_accuracy(y, y_pred):
         y_pred: is a tensor containing the network’s predictions
     Returns: a tensor containing the decimal accuracy of the prediction
     """
-    correct_pred = tf.argmax(y_pred, y)
+    correct_pred = tf.equal(tf.argmax(y_pred, 1), tf.argmax(y, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
     return accuracy
