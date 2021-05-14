3f595484fd8ec41c863c6fc6e9e112e07b4efd6e
daniloromero <1601@holbertonschool.com>
2021-05-14 00:28:47 +0000

    Added forward propagation graph method

---
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" creates the forward propagation graph for the neural network"""
+import tensorflow as tf
+
+
+create_layer = __import__('1-create_layer').create_layer
+
+
+def forward_prop(x, layer_sizes=[], activations=[]):
+    """creates the forward propagation graph for the neural network"""
+    for i in range(len(layer_sizes)):
+        x = create_layer(x, layer_sizes[i], activations[i])
+    return x
