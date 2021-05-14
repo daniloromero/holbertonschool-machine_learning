347e016f306f30f321bf632b7c0d9becf00f57c8
daniloromero <1601@holbertonschool.com>
2021-05-14 12:15:51 +0000

    Adjusment

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -4,7 +4,12 @@ import tensorflow as tf
 
 
 def calculate_accuracy(y, y_pred):
-    """ calculates the accuracy of a prediction"""
-    correct_pred = tf.argmax(y_pred, 1)
+    """ calculates the accuracy of a prediction
+    Args:
+        y: is a placeholder for the labels of the input data
+        y_pred: is a tensor containing the network’s predictions
+    Returns: a tensor containing the decimal accuracy of the prediction
+    """
+    correct_pred = tf.argmax(y_pred, y)
     accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
     return accuracy
