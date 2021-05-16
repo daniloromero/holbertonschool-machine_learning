e53e77110d2242cc1a898cf1b69ba4495aba04c3
daniloromero <1601@holbertonschool.com>
2021-05-16 02:30:32 +0000

    Model tested

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """ Module  builds, trains, and saves a neural network classifier"""
-import numpy as np
 import tensorflow as tf
 calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
 calculate_loss = __import__('4-calculate_loss').calculate_loss
