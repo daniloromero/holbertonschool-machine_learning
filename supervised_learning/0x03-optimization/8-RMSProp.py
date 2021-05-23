9d3125f2770d3f69528b744ac0588939a299c16f
Danilo Romero <60362073+daniloromero@users.noreply.github.com>
2021-05-22 23:31:31 -0500

    Pycodestyled

--- supervised_learning/0x03-optimization/8-RMSProp.py
+++ supervised_learning/0x03-optimization/8-RMSProp.py
@@ -12,7 +12,7 @@ def create_RMSProp_op(loss, alpha, beta2, epsilon):
         alpha is the learning rate
         beta2 is the RMSProp weight
         epsilon is a small number to avoid division by zero
-    Returns: the RMSProp optimization operation 
+    Returns: the RMSProp optimization operation
     """
     optimizer = tf.train.RMSPropOptimizer(alpha, decay=beta2, epsilon=epsilon)
     return optimizer.minimize(loss)
