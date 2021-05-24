cdd1b4b063ed8bada0a0c366811a5a7c2d64ae84
Danilo Romero <60362073+daniloromero@users.noreply.github.com>
2021-05-24 17:05:25 -0500

    First commit

---
+++ supervised_learning/0x04-error_analysis/0-create_confusion.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Module that creates a confusion matrix"""
+import numpy as np
+
+
+def create_confusion_matrix(labels, logits):
+    """creates a confusion matrix
+    Args:
+        labels: one-hot numpy.ndarray, shape (m, classes) contains
+         correct labels for each data point
+            m: is the number of data points
+            classes: is the number of classes
+
+        logits: one-hot numpy.ndarray shape (m, classes) with predicted labels
+    Returns: confusion numpy.ndarray shape (classes, classes) with row indices
+      showing the correct labels and column indices showing predicted labels
+    """
+    m, classes = labels.shape
+    conf_mat = np.zeros((classes, classes))
+    print(m)
+    print(conf_mat[5][3])
+    print(len(logits))
+    for i in range(m):
+        m_label = labels[i]
+        m_logit = logits[i]
+        x, *_ = np.where(m_label == 1)
+        y = np.where(m_logit == 1)
+        conf_mat[x[0]][y[0]] += 1
+    return conf_mat
