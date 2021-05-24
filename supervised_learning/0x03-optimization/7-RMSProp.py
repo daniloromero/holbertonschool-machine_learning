bf50974f85ba85c32452296069f4cd69a885c18c
Danilo Romero <60362073+daniloromero@users.noreply.github.com>
2021-05-23 21:33:58 -0500

    enclosed s in np.sqrt and then add epsilon

--- supervised_learning/0x03-optimization/7-RMSProp.py
+++ supervised_learning/0x03-optimization/7-RMSProp.py
@@ -15,5 +15,5 @@ def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     Returns: the updated variable and the new moment, respectively
     """
     s = (beta2 * s) + (1 - beta2) * (grad ** 2)
-    var -= (alpha * (grad / np.sqrt(s + epsilon)))
+    var -= (alpha * (grad / (np.sqrt(s) + epsilon)))
     return var, s
