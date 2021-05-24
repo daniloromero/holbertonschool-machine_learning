2a69482c18567ba4dc15639c5e75c6103c231100
Danilo Romero <60362073+daniloromero@users.noreply.github.com>
2021-05-23 20:29:46 -0500

    Added batch normalization with numpy

---
+++ supervised_learning/0x03-optimization/13-batch_norm.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""normalizes an unactivated output of a NN layer using batch normalization"""
+import numpy as np
+
+
+def batch_norm(Z, gamma, beta, epsilon):
+    """normalizes an unactivated output of NN layer using batch normalization
+    Args:
+        Z is a numpy.ndarray shape (m, n) that should be normalized
+            m is the number of data points
+            n is the number of features in Z
+        gamma: numpy.ndarray shape (1, n) with scales used for batch normal...
+        beta: numpy.ndarray shape (1, n) wit offsets used for batch normal...
+    epsilon is a small number used to avoid division by zero
+    Returns: the normalized Z matrix
+    """
+    mean = np.mean(Z)
+    variance = np.var(Z)
+    Z_norm = (Z - mean) / np.sqrt(variance + epsilon)
+    return gamma * Z_norm + beta
