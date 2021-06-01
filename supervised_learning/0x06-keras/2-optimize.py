e5496dbf56ccfe640b867df8973f71cf165ec8f8
Danilo Romero <60362073+daniloromero@users.noreply.github.com>
2021-06-01 12:26:39 -0500

    Update loss=categorical_crossentropy

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -16,6 +16,6 @@ def optimize_model(network, alpha, beta1, beta2):
     opt = K.optimizers.Adam(lr=alpha,  beta_1=beta1, beta_2=beta2)
     network.compile(
         optimizer=opt,
-
+        loss='categorical_crossentropy',
         metrics=['accuracy']
     )
