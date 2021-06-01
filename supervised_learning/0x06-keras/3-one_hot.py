c2315a7cca883a7956e28c6a2f16d50f52de3df5
Danilo Romero <60362073+daniloromero@users.noreply.github.com>
2021-06-01 11:06:50 -0500

    Add num_classes as parameter

--- supervised_learning/0x06-keras/3-one_hot.py
+++ supervised_learning/0x06-keras/3-one_hot.py
@@ -10,4 +10,4 @@ def one_hot(labels, classes=None):
         The last dimension of the one-hot matrix must be the number of classes
     Returns: the one-hot matrix
     """
-    return K.utils.to_categorical(labels, classes)
+    return K.utils.to_categorical(labels, num_classes=classes)
