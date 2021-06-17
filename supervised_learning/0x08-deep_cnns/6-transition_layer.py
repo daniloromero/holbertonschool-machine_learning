54245d4214e684c9b68de9f3ec7a3c9efad7ef83
Danilo Romero <60362073+daniloromero@users.noreply.github.com>
2021-06-16 19:27:45 -0500

    Add moduel that builds transition block

---
+++ supervised_learning/0x08-deep_cnns/6-transition_layer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Module  that builds a transition layer"""
+import tensorflow.keras as K
+
+
+def transition_layer(X, nb_filters, compression):
+    """builds a transition layer
+    Args:
+        X: is the output from the previous layer
+        nb_filters: is an integer representing the number of filters in X
+        compression: is the compression factor for the transition layer
+    Return: The output of the transition layer and the number of filters
+    """
+    init = K.initializers.he_normal(seed=None)
+    nb_filters = int(nb_filters * compression)
+    batch_norm = K.layers.BatchNormalization(axis=3)(X)
+
+    activation = K.layers.Activation('relu')(batch_norm)
+
+    conv1 = K.layers.Conv2D(
+        filters=nb_filters,
+        kernel_size=1,
+        padding='same',
+        strides=2,
+        kernel_initializer=init
+    )(activation)
+    l_avg_pool = K.layers.AveragePooling2D(pool_size=[7, 7],
+                                           strides=1,
+                                           padding='same')(conv1)
+
+    return l_avg_pool, nb_filters
