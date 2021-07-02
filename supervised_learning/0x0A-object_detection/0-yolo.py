79774f714f015bc2154554aad26d3291391b11b9
Danilo Romero <60362073+daniloromero@users.noreply.github.com>
2021-07-02 11:40:35 -0500

    Update pycodestyle

--- supervised_learning/0x0A-object_detection/0-yolo.py
+++ supervised_learning/0x0A-object_detection/0-yolo.py
@@ -5,7 +5,8 @@ import tensorflow.keras as K
 
 
 class Yolo():
-    """class Yolo that uses the Yolo v3 algorithm to perform object detection"""
+    """class Yolo that uses the Yolo v3 algorithm to perform object detection
+    """
 
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         """class Yolo that uses Yolo v3 algorithm to perform object detection
