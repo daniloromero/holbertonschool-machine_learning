13356d3a7a897aaa016c0966ad12f2e5f5a872a1
Danilo Romero <60362073+daniloromero@users.noreply.github.com>
2021-07-04 22:54:54 -0500

    Update add preprocess call for train and test datasets

--- supervised_learning/0x09-transfer_learning/0-transfer.py
+++ supervised_learning/0x09-transfer_learning/0-transfer.py
@@ -2,8 +2,18 @@
 """Transfer learning"""
 
 import tensorflow.keras as K
-import numpy as np
-import matplotlib.pyplot as plt
+import tensorflow as tf
+
+
+def preprocess_data(X, Y):
+    """
+    This method pre-processes the data
+    for the model
+    """
+    X_p = K.applications.inception_v3.preprocess_input(X)
+    Y_p = K.utils.to_categorical(Y, num_classes=10)
+    return (X_p, Y_p)
+
 
 if __name__ == '__main__':
 
@@ -12,6 +22,8 @@ if __name__ == '__main__':
     and save it in a file cifar10.h5
     """
     (x_train, y_train), (x_test, y_test) = K.datasets.cifar10.load_data()
+    x_train, y_train = preprocess_data(x_train, y_train)
+    x_test, y_test = preprocess_data(x_test, y_test)
 
     base_model = K.applications.InceptionV3(
         include_top=False,
@@ -48,13 +60,3 @@ if __name__ == '__main__':
     )
 
     model.save('cifar10.h5')
-
-
-def preprocess_data(X, Y):
-    """
-    This method pre-processes the data
-    for the model
-    """
-    X_p = K.applications.inceptionv3.preprocess_input(X)
-    Y_p = K.utils.to_categorical(Y, num_classes=10)
-    return (X_p, Y_p)
