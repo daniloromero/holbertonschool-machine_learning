43e73b988e935d0861dcd39a5fc48318e028736c
daniloromero <1601@holbertonschool.com>
2021-07-22 21:09:59 +0000

    Update inverse for single matrix

--- math/0x05-advanced_linear_algebra/4-inverse.py
+++ math/0x05-advanced_linear_algebra/4-inverse.py
@@ -88,7 +88,7 @@ def inverse(matrix):
         if len(matrix[0]) == 0:
             return None
         if len(matrix[0]) == 1:
-            return 1 / matrix[0][0]
+            return [[1 / matrix[0][0]]]
 
     if len(matrix) == 2:
         adjugt = [[matrix[1][1], -matrix[0][1]], [-matrix[1][0], matrix[0][0]]]
