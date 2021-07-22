497c32a654180219f921039f2846323134f6faa5
daniloromero <1601@holbertonschool.com>
2021-07-22 22:45:38 +0000

    Add matrix definiteness module

---
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Module that calculates the definiteness of a matrix"""
+import numpy as np
+
+
+def definiteness(matrix):
+    """calculates the definiteness of a matrix
+    Args:
+        matrix: numpy.ndarray of shape (n, n)
+    Returns: the string Positive definite, Positive semi-definite, Negative
+    semi-definite, Negative definite, or Indefinite, respectively
+    """
+    if type(matrix) is not np.ndarray:
+        raise TypeError('matrix must be a numpy.ndarray')
+
+    if len(matrix.shape) == 1:
+        return None
+    if matrix.shape != matrix.T.shape:
+        return None
+    w, v = np.linalg.eig(matrix)
+
+    if np.all(w > 0):
+        return 'Positive definite'
+    if np.all(w >= 0):
+        return 'Positive semi-definite'
+    if np.all(w <= 0):
+        return 'Negative semi-definite'
+    if np.all(w < 0):
+        return 'Negative definite'
+    if np.any(w > 0) and np.any(w < 0):
+        return 'Indefinite'
