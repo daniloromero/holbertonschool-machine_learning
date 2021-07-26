979a478b9c9bc9dc21f6cb71f016142810574273
daniloromero <1601@holbertonschool.com>
2021-07-26 17:08:41 +0000

    Update type od input must be numpy.ndarray

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -13,7 +13,7 @@ def mean_cov(X):
         mean is a numpy.ndarray of shape(1, d) containing the mean
         cov is a numpy.ndarray of shape(d, d) containing covariance matrix
     """
-    if len(X.shape) != 2:
+    if type(X) is not np.ndarray and len(X.shape) != 2:
         raise TypeError('X must be a 2D numpy.ndarray')
     if X.shape[0] < 2:
         raise ValueError('X must contain multiple data points')
