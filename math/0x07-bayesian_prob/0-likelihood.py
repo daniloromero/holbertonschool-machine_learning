fb62ceda1784947d0723572ecadb010dc463c641
daniloromero <1601@holbertonschool.com>
2021-07-29 23:57:12 +0000

    Update steps for factorial

--- math/0x07-bayesian_prob/0-likelihood.py
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -18,10 +18,13 @@ def likelihood(x, n, P):
             'x must be an integer that is greater than or equal to 0')
     if x > n:
         raise ValueError('x cannot be greater than n')
-    if type(P) != np.ndarray or len(np.shape(P)) != 1:
+    if type(P) != np.ndarray or len(P.shape) != 1:
         raise TypeError('P must be a 1D numpy.ndarray')
     if np.any(P < 0) or np.any(P > 1):
         raise ValueError('All values in P must be in the range [0, 1]')
-    L = ((np.math.factorial(n)) / (np.math.factorial(x)
-         (np.math.factorial(n - x))) * P ** x * (1 - P) ** (n - x))
+    top = (np.math.factorial(n))
+    bottom = (np.math.factorial(x) * np.math.factorial(n - x))
+    factorial = top / bottom
+    L = factorial * (np.power(P, x)) * (np.power((1 - P), (n - x)))
+
     return L
