cb2c3c2e705fe152726994fbea74c5a0c6926491
Danilo Romero <60362073+daniloromero@users.noreply.github.com>
2021-08-04 19:48:03 -0500

    Add conditions for input parameters

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -12,5 +12,9 @@ def initialize(X, k):
         k is a positive integer giving the number of clusters
     Returs: numpy.ndarray shape(k, d) with initializes centroids, None if fails
     """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+    if type(k) != int or k <= 0 or k >= X.shape[0]:
+        return None
     n, d = X.shape
     return np.random.uniform(np.min(X, axis=0), np.max(X, axis=0), (k, d))
