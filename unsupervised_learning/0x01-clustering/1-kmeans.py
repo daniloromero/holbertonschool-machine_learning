3c23fd69ba322ca9b24c5b278e96a9cfe5fa9225
Danilo Romero <60362073+daniloromero@users.noreply.github.com>
2021-08-05 17:57:25 -0500

    Add task

---
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""Module tha performs K-means on a dataset"""
+import numpy as np
+
+
+def kmeans(X, k, iterations=1000):
+    """performs K-means on a dataset
+    Args:
+        X is numpy.ndarray shape(n, d) containing dataset fo clustering
+            n is the number of data points
+            d is the number of dimensions
+        k is a positive integer giving the number of clusters
+        iterations is a positive integer with max number of iterations
+    Return: C, clss, or None, None on failure
+        C: a numpy.ndarray of shape (k, d) with centroid means for each cluster
+        clss: a numpy.ndarray of shape (n,) containing the index of the cluster
+            in C that each data point belongs to
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None
+    if type(k) != int or k <= 0 or k >= X.shape[0]:
+        return None, None
+    if type(iterations) != int or iterations <= 0:
+        return None, None
+    n, d = X.shape
+    # initialize random centroids
+    X_min = np.min(X, axis=0)
+    X_max = np.max(X, axis=0)
+    C = np.random.uniform(X_min, X_max, (k, d))
+
+    for i in range(iterations):
+        C_clone = np.copy(C)
+        # Calculate distance betwen points X and centroids C
+        dist = np.linalg.norm(X - C[:, np.newaxis], axis=2)
+        # one hot encoding matrix: datapoint to closest centroid
+        clss = np.argmin(dist, axis=0)
+        print(X[clss == k])
+        # Update centroids
+        for k in range(C.shape[0]):
+            # reinitialize centroid If cluster contains no data points
+            if (X[clss == k].size == 0):
+                C[k, :] = np.random.uniform(X_min, X_max, (1, d))
+            else:
+                C[k, :] = np.mean(X[clss == k], axis=0)
+        # If no change in the cluster centroids the return
+        if (C_clone == C).all():
+            print(i)
+            return (C, clss)
+
+    return (C, clss)
