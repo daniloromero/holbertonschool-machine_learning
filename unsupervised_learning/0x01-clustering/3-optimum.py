add1350ea74a06a0cf3b0bd258a6e1ed4dea29f5
Danilo Romero <60362073+daniloromero@users.noreply.github.com>
2021-08-05 19:29:55 -0500

    Add task

---
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Module tha tests for the optimum number of clusters by variance"""
+import numpy as np
+kmeans = __import__('1-kmeans').kmeans
+variance = __import__('2-variance').variance
+
+
+def optimum_k(X, kmin=1, kmax=None, iterations=1000):
+    """ Test for the optimum number of clusters by variance
+    Arg:
+        X: np.ndarray of shape (n, d) containing the data set
+        kmin: positive int, minimum number of clusters to check
+                for (inclusive)
+        kmax: positive int, maximum number of clusters to check
+        iterations: is a positive integer containing the maximum
+                    number of iterations for K-means
+    Returns: (results, d_vars) or (None, None) on failure
+        - results: list, outputs of K-means for each cluster size
+        - d_vars: list, with the difference in varianca from smallest
+                    cluster size for each cluster size
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None
+    if type(kmin) != int or kmin <= 0 or kmin >= X.shape[0]:
+        return None, None
+    if type(kmax) != int or kmax <= 0 or kmax >= X.shape[0]:
+        return None, None
+    if kmin >= kmax:
+        return None, None
+    if type(iterations) != int or iterations <= 0:
+        return None, None
+    results = []
+    d_vars = []
+    C_kmin, _ = kmeans(X, kmin)
+    kmin_var = variance(X, C_kmin)
+
+    for k in range(kmin, kmax + 1):
+        C_k, clss = kmeans(X, k)
+        results.append((C_k, clss))
+        k_var = variance(X, C_k)
+        d_vars.append(kmin_var - k_var)
+    return (results, d_vars)
