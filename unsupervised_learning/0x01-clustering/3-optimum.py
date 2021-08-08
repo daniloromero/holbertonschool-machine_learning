38748e88aafa425708300f6be730953c7c0c8266
Danilo Romero <60362073+daniloromero@users.noreply.github.com>
2021-08-08 17:40:05 -0500

    Update

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -38,8 +38,8 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
         for k in range(kmin, kmax + 1):
             C_k, clss = kmeans(X, k)
             results.append((C_k, clss))
-            k_var = variance(X, C_k)
-            d_vars.append(kmin_var - k_var)
+            d_vars.append(kmin_var - variance(X, C_k))
         return results, d_vars
+
     except Exception:
         return None, None
