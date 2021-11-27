9286779f1cd3980dd43b3edfbf0cac3195920afc
Danilo Romero <60362073+daniloromero@users.noreply.github.com>
2021-11-27 18:55:16 -0500

    Update add current unix time conditional to get next upcoming launch

--- pipeline/0x01-apis/3-upcoming.py
+++ pipeline/0x01-apis/3-upcoming.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python3
 """Script that print the location of a user requested to Github API"""
 import requests
-
+import time
 
 if __name__ == '__main__':
     url = 'https://api.spacexdata.com/v4/launches/upcoming/'
     r = requests.get(url)
     answer = r.json()
     # sort lauches by date_unix field and get lauch name and time
-    sorted_launches = sorted(answer, key=lambda d: d['date_unix'])
-    launch_name = sorted_launches[0]['name']
-    launch_time = sorted_launches[0]['date_local']
+    sorted_launches = sorted(answer, key=lambda d: int(d['date_unix']))
+    # get current time to search for nexxt upcomig launch in sorted array
+    now = round(time.time())
+    for i in range(len(sorted_launches)):
+        if sorted_launches[i]['date_unix'] >= now:
+            launch_name = sorted_launches[i]['name']
+            launch_time = sorted_launches[i]['date_local']
+            break
     # Request rocket endpoint by id and get rocket name
-    rocket_id = sorted_launches[0]['rocket']
+    rocket_id = sorted_launches[i]['rocket']
     rocket_r = requests.get('https://api.spacexdata.com/v4/rockets/'
                             + rocket_id)
     rocket_name = rocket_r.json()['name']
     # Request launchpads endpoint and get launchpad name and locality
-    launchpad_id = sorted_launches[0]['launchpad']
+    launchpad_id = sorted_launches[i]['launchpad']
     launchpad_r = requests.get('https://api.spacexdata.com/v4/launchpads/'
                                + launchpad_id)
     launchpad_name = launchpad_r.json()['name']
