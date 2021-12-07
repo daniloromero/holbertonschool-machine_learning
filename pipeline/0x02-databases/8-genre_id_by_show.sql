98390a561365d5b68f6aa826698833528adfcbb6
Danilo Romero <60362073+daniloromero@users.noreply.github.com>
2021-12-06 22:35:47 -0500

    Update name field and add order

--- pipeline/0x02-databases/8-genre_id_by_show.sql
+++ pipeline/0x02-databases/8-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 --  lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
-SELECT tv_shows.title, tv_genres.id FROM tv_shows, tv_show_genres, tv_genres
+SELECT tv_shows.title, tv_genres.id AS genre_id FROM tv_shows, tv_show_genres, tv_genres
 WHERE tv_show_genres.show_id = tv_shows.id AND tv_show_genres.genre_id = tv_genres.id
 -- SELECT tv_shows.title, tv_genres.id AS genre_id FROM tv_show_genres
 -- JOIN tv_shows ON tv_show_genres.show_id=tv_shows.id
