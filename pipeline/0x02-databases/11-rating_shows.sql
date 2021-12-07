514b37c0b31e01179d454552c2646a47e44eec88
Danilo Romero <60362073+daniloromero@users.noreply.github.com>
2021-12-07 18:27:15 -0500

    Update order statement with rating column output name

--- pipeline/0x02-databases/11-rating_shows.sql
+++ pipeline/0x02-databases/11-rating_shows.sql
@@ -3,4 +3,4 @@ SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_show_ratings, tv_shows
 WHERE tv_show_ratings.show_id = tv_shows.id
 GROUP BY title
-ORDER BY rating_sum DESC;
+ORDER BY rating DESC;
--- pipeline/0x02-databases/12-rating_genres.sql
+++ pipeline/0x02-databases/12-rating_genres.sql
@@ -4,4 +4,4 @@ LEFT JOIN tv_shows ON tv_shows.id = tv_show_ratings.show_id
 INNER JOIN tv_show_genres on tv_show_genres.show_id = tv_shows.id
 INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY name
-ORDER BY rating_sum DESC;
+ORDER BY rating DESC;
