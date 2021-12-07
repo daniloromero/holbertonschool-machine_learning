d153384bdf4c392630bf2d340f2334c3c4b8f6bb
Danilo Romero <60362073+daniloromero@users.noreply.github.com>
2021-12-07 18:24:15 -0500

    Update name fo rating output column

--- pipeline/0x02-databases/11-rating_shows.sql
+++ pipeline/0x02-databases/11-rating_shows.sql
@@ -1,5 +1,6 @@
 --  lists all shows from hbtn_0d_tvshows_rate by their rating
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating_sum FROM tv_show_ratings, tv_shows
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+FROM tv_show_ratings, tv_shows
 WHERE tv_show_ratings.show_id = tv_shows.id
 GROUP BY title
 ORDER BY rating_sum DESC;
