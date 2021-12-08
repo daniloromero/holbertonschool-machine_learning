cfd6a96024d69ca42c1ed15d45921f8e830f7fbe
Danilo Romero <60362073+daniloromero@users.noreply.github.com>
2021-12-07 22:08:20 -0500

    Update filter parameter in nginx.xcoountdocumets

--- pipeline/0x02-databases/34-log_stats.py
+++ pipeline/0x02-databases/34-log_stats.py
@@ -6,7 +6,7 @@ from pymongo import MongoClient
 if __name__ == '__main__':
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx = client.logs.nginx
-    logs = nginx.count_documents()
+    logs = nginx.count_documents({})
     get = nginx.count_documents({'method': 'GET'})
     post = nginx.count_documents({'method': 'POST'})
     put = nginx.count_documents({'method': 'PUT'})
