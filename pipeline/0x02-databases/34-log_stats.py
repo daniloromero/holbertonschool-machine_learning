6b8b1aefcd4c0864a17701adee0c4d53e03de872
Danilo Romero <60362073+daniloromero@users.noreply.github.com>
2021-12-07 22:06:04 -0500

    Update count_documents() in nginx collection

--- pipeline/0x02-databases/34-log_stats.py
+++ pipeline/0x02-databases/34-log_stats.py
@@ -6,14 +6,14 @@ from pymongo import MongoClient
 if __name__ == '__main__':
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx = client.logs.nginx
-    logs = nginx.find()
+    logs = nginx.count_documents()
     get = nginx.count_documents({'method': 'GET'})
     post = nginx.count_documents({'method': 'POST'})
     put = nginx.count_documents({'method': 'PUT'})
     patch = nginx.count_documents({'method': 'PATCH'})
     delete = nginx.count_documents({'method': 'DELETE'})
     status = nginx.count_documents({'method': 'GET', 'path': '/status'})
-    print("{} logs".format(logs.count_documents()))
+    print("{} logs".format(logs))
     print("Methods:\n" +
           "\tmethod GET: {}\n".format(get) +
           "\tmethod POST: {}\n".format(post) +
